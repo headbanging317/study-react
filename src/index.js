773751a3704c010ae179c2603a1214e952c8a7a2
headbanging317 <24thelf@gmail.com>
2017-11-26 20:00:37 +0900

    start react study

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App';
+
+const rootElement = document.getElementById('root');
+
+ReactDom.render(<App/>, rootElement);
+
+if(module.hot) {
+    module.hot.accept();
+}
